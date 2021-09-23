e5a09a7b5e2f7562573b17ca4b63f48f04dd8595
YBAKSH10 <y.bakshi.10@student.scu.edu.au>
2021-09-23 20:51:20 +0530

    Create TestCase2.java

---
+++ src/test/java/ass3/mygame2/TestCase2.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Yamini and Umer Jahangir
+ */
+public class TestCase2 {
+
+    private static String getPlayerItem() {
+    throw new UnsupportedOperationException("Not supported yet."); 
+//To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public TestCase2() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+     }
+    
+    @AfterEach
+    public void tearDown() {
+       }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testPlayerItem() {
+     String expected = "remove item";
+     // An item can be added or removed as well
+          String actual=TestCase2.getPlayerItem();
+     assertEquals(expected,actual);
+          }
+}
